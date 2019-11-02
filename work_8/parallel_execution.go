8aa37e050b0d79a79e397197945e3dc33328b38e
br9k <maxim@tetra-soft.ru>
2019-11-02 23:26:48 +0300

    теперь запускается указанное число горутин, которым мы по каналу пердаем задачи

--- work_8/parallel_execution.go
+++ work_8/parallel_execution.go
@@ -4,52 +4,98 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+// CustomTimeEncoder function of own formulating time for output to the log
+func CustomTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+}
+
+func init() {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeTime = CustomTimeEncoder
+	config.Encoding = "console"
+	var err error
+	var logger *zap.Logger
+	if logger, err = config.Build(); err != nil {
+		zap.L().Fatal("Logger create error", zap.Error(err))
+	}
+	zap.ReplaceGlobals(logger)
+}
 
 //ParalelWorkMain основная функция в которую передаем slcie задач
 //мыксимальное число выполняемых паралельно задач и максимальное число ошибок
 func ParalelWorkMain(works []func() error, maxParalelWorks, maxErrors int) {
 	fmt.Fprintf(os.Stdout, "Функция запущена со слайсом %d задач, паралельно будет выполнятся %d, до %d ошибок\n", len(works), maxParalelWorks, maxErrors)
-	worksInProgess := make(chan func() error, maxParalelWorks-1)
-	worksInProgessCounter := make(chan interface{}, maxParalelWorks-1)
+	worksChanel := make(chan func() error)
+	errorChanel := make(chan error, maxParalelWorks-1)
 	chanelForEmergancyStop := make(chan interface{})
 
-	var errorsCount int
-	var m sync.RWMutex
-
+	var (
+		work                  func() error
+		workerWait, errorWait sync.WaitGroup
+	)
+	errorWait.Add(1)
 	go func() {
-	OUT:
-		for i, work := range works {
-			select {
-			case worksInProgess <- work: // отсюда будем брать задачи
-				fmt.Fprintf(os.Stdout, "В очередь задач добавлена задача номер %d\n", i+1)
-				worksInProgessCounter <- nil // а этот канал будет нас блокировать пока не выполнится одна из задач
-
-			case <-chanelForEmergancyStop:
-				fmt.Fprintf(os.Stderr, "Закончили передавать задачи в канал потому что слишком много ошибок\n")
-				break OUT
-			}
-		}
-		close(worksInProgess)
+		errorCounter(errorChanel, chanelForEmergancyStop, maxErrors)
+		errorWait.Done()
 	}()
 
-	for work := range worksInProgess {
-		go func(workFunc func() error) {
-			if err := workFunc(); err != nil {
-				m.RLock()
-				errorsCount++
-				if errorsCount == maxErrors {
-					fmt.Fprintf(os.Stderr, "Количество ошибок по работам равно %d выходим из программы\n", maxErrors)
-					chanelForEmergancyStop <- nil
-				}
-				m.RUnlock()
-			}
-			<-worksInProgessCounter
-		}(work)
+	for i := 0; i < maxParalelWorks; i++ {
+		workerWait.Add(1)
+		go func() {
+			worker(worksChanel, chanelForEmergancyStop, errorChanel)
+			workerWait.Done()
+		}()
 	}
+OUT:
+	for _, work = range works {
+		select {
+		case worksChanel <- work:
+		case <-chanelForEmergancyStop:
+			break OUT
+		}
+	}
+	close(worksChanel)
+	workerWait.Wait()
+	close(errorChanel)
+	errorWait.Wait()
+}
+
+func worker(workChanel <-chan func() error, stopChanel <-chan interface{}, errorChanel chan<- error) {
+	var ok bool
+	var workFunc func() error
 	for {
-		if len(worksInProgessCounter) == 0 || errorsCount >= maxErrors {
-			break
+		select {
+		case workFunc, ok = <-workChanel:
+			if !ok {
+				return
+			}
+			errorChanel <- workFunc()
+		case <-stopChanel:
+			return
+		}
+	}
+}
+
+func errorCounter(errorChanel <-chan error, stopChan chan<- interface{}, maximumNumberOfErrors int) {
+	var workDone, errCount int
+	var err error
+	for err = range errorChanel {
+		workDone++
+		if err != nil {
+			errCount++
+		}
+		if errCount == maximumNumberOfErrors-1 {
+			fmt.Printf("Достигнуто максимальное число ошибок %d\n", maximumNumberOfErrors)
+			close(stopChan)
+			for range errorChanel {
+			}
+			return
 		}
 	}
 }
--- work_8/parallel_execution_test.go
+++ work_8/parallel_execution_test.go
@@ -9,21 +9,13 @@ import (
 	"time"
 )
 
-//fakeWork функция которая якобы над чем то работает
-// func fakeWork (workDone,stop <-chanel,workNameForTest string) {
-
-// 	time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-// }
-
 func TestParallelWOrk(t *testing.T) {
-	// t.Skip()
 	totalWorkNumber := 50
 	rand.Seed(int64(time.Now().Nanosecond()))
 	works := make([]func() error, totalWorkNumber)
 	for i := 0; i < totalWorkNumber; i++ {
 		works[i] = func() error {
 			wait := rand.Intn(1e3)
-			// fmt.Fprintf(os.Stderr, "Работа началась %d \n", wait)
 			time.Sleep(time.Duration(wait) * time.Millisecond)
 			if wait > 800 {
 				fmt.Fprintf(os.Stderr, "Работа выолнялась долго %d млисекунд и вернула ошибку \n", wait)
@@ -32,7 +24,6 @@ func TestParallelWOrk(t *testing.T) {
 			fmt.Fprintf(os.Stdout, "Работы выолнялась %d млисекунд\n", wait)
 			return nil
 		}
-
 	}
 	ParalelWorkMain(works, 10, 3)
 	ParalelWorkMain(works, 20, 20)
